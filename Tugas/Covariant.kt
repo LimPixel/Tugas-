7c0d40be2abbcb197238d06412c7326a0ec3fa73
NeoFreya <68523632+NeoFreya@users.noreply.github.com>
2021-09-02 10:01:53 +0700

    Tugas
    Tugas

---
+++ Tugas/Covariant.kt
@@ -0,0 +1,14 @@
+class ProofCovariant<out T>(val data: T) {
+
+    fun data(): T {
+        return data
+    }
+}
+
+fun main() {
+    val covariantString = ProofCovariant<String>("Eko")
+    val covariantAny: ProofCovariant<Any> = covariantString
+
+    println(covariantAny.data())
+}
+
